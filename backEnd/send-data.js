7a6c57fc33b8a516bbe15e158fdfc241a9917dc0
ThanhNghia17-py <nguyenthanhnghialang17py@gmail.com>
2025-06-19 15:45:31 +0700

    all

---
+++ backEnd/send-data.js
@@ -0,0 +1,14 @@
+const axios = require("axios");
+
+const data = {
+  number1: 123,
+  number2: 456
+};
+
+axios.post("https://opcua-1.onrender.com/data", data)
+  .then(response => {
+    console.log("✅ Đã gửi dữ liệu thành công:", response.data);
+  })
+  .catch(error => {
+    console.error("❌ Lỗi khi gửi dữ liệu:", error.message);
+  });
